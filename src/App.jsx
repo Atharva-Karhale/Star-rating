0c7e9ab672dc336b93faf4936022ed248da8e520
Atharva-Karhale <pirateask6@gmail.com>
2024-03-05 11:36:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ExpandCard from "./components/ExpandCard"
+
+
+function App() {
+
+
+
+  return (
+    <div className=' bg-[#1a1a1a] p-4'>
+      <ExpandCard/>
+    </div>
+  )
+}
+
+export default App
