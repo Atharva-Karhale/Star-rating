a2ce722f0848a32d2be1787b9074940ff7bf4d96
Atharva-Karhale <pirateask6@gmail.com>
2024-03-06 16:54:59 +0530

    added copy code functionality and added hex and rgb codde formats

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-#Background changer
+#Color generator Website
+
+-Generates random colors across the full spectrum
+-Displays the color code in both HEX and RGB format
+-Easily copy the codes to the clipboard in single click
 
 -JavaScript Practice
 -Day - 4/30 ✅
+
+![demo_img1](image.png)
+![demo_img2](image-1.png)
 
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react'
 
 function RandomColor() {
 
-    const [color, setColor] = useState ("black")
+    const [color, setColor] = useState ("#000000")
+    const [rgbcolor, setRGBColor] = useState ({R:0, G:0, B:0})
 
     const a = [1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
 
@@ -15,7 +16,28 @@ function RandomColor() {
             newColor = newColor + a[randomIndex]
         }
         setColor(newColor)
+        
+            let newwColor = newColor.slice(1)
+
+            let R = parseInt(newwColor.substring(0,2),16)
+            let G = parseInt(newwColor.substring(2,4),16)
+            let B = parseInt(newwColor.substring(4,6),16)
+
+            setRGBColor( {R, G, B} )
+            
+        }
+
+    const copyHex = () => {
+        navigator.clipboard.writeText(color)
+        alert("Hex code copied to clipboard!")
     }
+
+    const copyRGB = () => {
+        navigator.clipboard.writeText(`R:${rgbcolor.R}, G:${rgbcolor.G}, B:${rgbcolor.B}`)
+        alert("RGB code copied to clipboard!")
+    }
+    
+    
 
   return (
 
@@ -24,11 +46,15 @@ function RandomColor() {
     className={`flex items-center h-screen flex-col `} >
         <button 
         onClick={changeColor}
-        className=' mt-8 py-2 px-4 border-4 border-[#1b1b1b] bg-[#fff] shadow-lg shadow-[#60ddff] rounded-lg w-fit'
+        className=' mt-8 py-2 px-4 border-4 border-[#1b1b1b] bg-[#fff] shadow-lg shadow-[#60ddff] rounded-lg w-fit mb-10 font-bold underline'
         >
         Random Color
         </button>
-        {console.log(color)}
+        <div className=' bg-white px-4 py-2 rounded-lg border-2 border-[#2f2f2f] flex flex-col justify-between items-center text-center'>
+        <span className=' font-bold text-xl'>Color codes - </span><br/>
+        Hex Code = [{color}] <button className=' text-white border-4 border-[#95bde2] py-1 px-2 rounded-lg w-full underline bg-[#042846]' onClick={copyHex}>COPY</button><br/>
+        RGB Code = [R:{rgbcolor.R}, G:{rgbcolor.G}, B:{rgbcolor.B}] <button className=' text-white border-4 border-[#95bde2] py-1 px-2 rounded-lg w-full underline bg-[#042846]' onClick={copyRGB}>COPY</button>
+        </div>
         
     </div>
   )
